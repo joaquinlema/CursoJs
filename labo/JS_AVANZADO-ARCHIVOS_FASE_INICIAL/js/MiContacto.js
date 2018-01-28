3ad55bb8fa36a7b1ae81bdbaaed8d3531eef0902
joaquin lema <joaco.lema@hotmail.com>
2018-01-28 15:23:57 -0300

    procesamiento de mail

--- labo/JS_AVANZADO-ARCHIVOS_FASE_INICIAL/js/MiContacto.js
+++ labo/JS_AVANZADO-ARCHIVOS_FASE_INICIAL/js/MiContacto.js
@@ -8,6 +8,7 @@ function activado() {
 	var nodo_mens = formulario.querySelector('textarea[name = mensaje]')
 
 	nodo_nombre.addEventListener('blur',validarNombre)
+	nodo_email.addEventListener('blur',validarEmail)
 	formulario.addEventListener('submit',validar)
 
 }
@@ -22,24 +23,65 @@ function validarNombre(argument) {
 
 	var nombreValor = argument.target
 
-	if(nombreValor.value.length > 4 && isNaN(nombreValor.value.charAt(0)) ){
-		console.log(validarClase(nombreValor))
-		nombreValor.classList.add("ok")
+	if(tamañoElem(nombreValor) > 4 && isNaN(nombreValor.value.charAt(0)) ){
+		
+		agregarClase(nombreValor,"ok")
 
 	}else {
-		
-		nombreValor.classList.add("error")	
-	}
-}
 
-function validarMail(argument) {
-	// body...
+		if(contieneClase(nombreValor,"ok")){
+		
+			quitarClase(nombreValor,"ok")
+		
+		}
+
+		agregarClase(nombreValor,"error")
+
+	}
 }
 
 function validarMens(argument) {
 	// body...
 }
 
-function validarClase(elemento){
-	return elemento.classList.contains("ok")
+function validarEmail(argument) {
+
+	var emailValor = argument.target
+
+	if (esMail(emailValor.value)) {
+		
+		agregarClase(emailValor,"ok")
+		
+	}else {
+		
+		if(contieneClase(emailValor,"ok")){
+				
+			quitarClase(emailValor,"ok")
+				
+		}
+		
+		agregarClase(emailValor,"error")
+		
+	}
+}
+
+function esMail(params) {
+	var filter = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;	
+	return String(params).search (filter) != -1;
+}
+
+function agregarClase(elemento,nombreClase) {
+	return elemento.classList.add(nombreClase)
+}
+
+function quitarClase(elemento,nombreClase) {
+	return elemento.classList.remove(nombreClase)
+}
+
+function tamañoElem(params) {
+	return params.value.length
+}
+
+function contieneClase(elemento,nombreClase){
+	return elemento.classList.contains(nombreClase)
 }
