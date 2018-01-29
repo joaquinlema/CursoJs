44e9efaa37bde1e52b27996b5505b5d65a35d4c6
joaquin lema <joaco.lema@hotmail.com>
2018-01-29 15:31:39 -0300

    funcionapeticionajax

--- labo/JS_AVANZADO-ARCHIVOS_FASE_INICIAL/js/AJAXLab.js
+++ labo/JS_AVANZADO-ARCHIVOS_FASE_INICIAL/js/AJAXLab.js
@@ -1,3 +1,34 @@
-window.load = function() {
-    alert("hola")
+window.onload = function() {
+    var ajax;
+    iniciar()
+}
+
+//window.addEventListener('load',iniciar)
+
+function iniciar() {
+    
+    var menu = document.querySelectorAll("#menu li")
+   
+    menu.forEach(function(element) {
+        element.addEventListener('click',peticionAjax)
+    }, this);
+}
+
+function peticionAjax(params) {
+
+    params.preventDefault()
+
+    var metodo = "GET"
+    var archivo = params.target.href//especifico el archivo que  quiero
+
+    ajax = new XMLHttpRequest(); //creo un objeto para realizar peticiones ajax
+    ajax.open(metodo,archivo) // especifico el tipo de petitorio y el archivo al cual le voy a pedir al servidor
+    ajax.addEventListener("load",procesarRespuesta)//especifico que hacer cuando me responde el servidor 
+    ajax.send()//envio la consulta
+}
+
+function procesarRespuesta() {
+
+    var caja = document.querySelector('#page')
+    caja.innerHTML = ajax.responseText;
 }
