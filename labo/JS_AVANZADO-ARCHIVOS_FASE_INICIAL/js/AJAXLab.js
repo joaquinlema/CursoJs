a67ee8a50637c53a785437ce5fd4d02c6c691c06
joaquin lema <joaco.lema@hotmail.com>
2018-01-29 15:36:12 -0300

    refactor

--- labo/JS_AVANZADO-ARCHIVOS_FASE_INICIAL/js/AJAXLab.js
+++ labo/JS_AVANZADO-ARCHIVOS_FASE_INICIAL/js/AJAXLab.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+/*window.onload = function() {
     var ajax;
     iniciar()
 }
-
-//window.addEventListener('load',iniciar)
+*/
+window.addEventListener('load',iniciar)
 
 function iniciar() {
     
@@ -21,14 +21,14 @@ function peticionAjax(params) {
     var metodo = "GET"
     var archivo = params.target.href//especifico el archivo que  quiero
 
-    ajax = new XMLHttpRequest(); //creo un objeto para realizar peticiones ajax
+    var ajax = new XMLHttpRequest(); //creo un objeto para realizar peticiones ajax
     ajax.open(metodo,archivo) // especifico el tipo de petitorio y el archivo al cual le voy a pedir al servidor
     ajax.addEventListener("load",procesarRespuesta)//especifico que hacer cuando me responde el servidor 
     ajax.send()//envio la consulta
 }
 
-function procesarRespuesta() {
+function procesarRespuesta(elementoAjax) {
 
     var caja = document.querySelector('#page')
-    caja.innerHTML = ajax.responseText;
+    caja.innerHTML = elementoAjax.target.responseText;
 }
